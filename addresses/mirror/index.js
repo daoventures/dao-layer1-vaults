0908a3df88458f0a08100623d05755e8b0203d5c
seshanthS <>
2021-09-08 10:01:29 +0530

    Add test - mAMZN_UST

--- addresses/mirror/index.js
+++ addresses/mirror/index.js
@@ -3,15 +3,18 @@ module.exports = {
         TOKENS: {
             WETH: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
             mTSLA:"0x21cA39943E91d704678F5D00b6616650F066fD63",
+            mAMZN:"0x0cae9e4d663793c2a2a0b211c1cf4bbca2b9caa7",
             UST:"0xa47c8bf37f92aBed4A126BDA807A7b7498661acD",
         },
 
         LPTOKENS: {
             mTSLA_UST: "0x5233349957586A8207c52693A959483F9aeAA50C",
+            mAMZN_UST: "0x0Ae8cB1f57e3b1b7f4f5048743710084AA69E796",
         },
 
         MIRROR: {
             mTSLA_UST_POOL:"0x43DFb87a26BA812b0988eBdf44e3e341144722Ab",
+            mAMZN_UST_POOL:"0x1fABef2C2DAB77f01053E9600F70bE1F3F657F51",
         },
 
         ADDRESSES: {
